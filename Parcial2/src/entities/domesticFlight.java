93685b52bf464dfc59c66f0a66eae68d3891fde4
Heller15 <hellerjmartinez@unicesar.edu.co>
2026-05-25 17:48:36 -0500

    preparar todo para el booking

--- Parcial2/src/entities/domesticFlight.java
+++ Parcial2/src/entities/domesticFlight.java
@@ -5,8 +5,8 @@ public class domesticFlight extends Flies{
     private double estimatedDuration;
     private String includesFood;
 
-    public domesticFlight(String codeFly, String cityOrigin, String cityDestination, String dateFly, String hourDeparture, String hourArrive, long capacity, int seatsAvailable, double basePrice, String statusFly, double estimatedDuration, String includesFood) {
-        super(codeFly, cityOrigin, cityDestination, dateFly, hourDeparture, hourArrive, seatsAvailable, basePrice, statusFly);
+    public domesticFlight(String codeFly, String cityOrigin, String cityDestination, String dateFly, String hourDeparture, String hourArrive, int seatsAvailable, double basePrice, String statusFly, double estimatedDuration, String includesFood) {
+        super(codeFly, cityOrigin, cityDestination, dateFly, hourDeparture, hourArrive, basePrice, statusFly);
         this.estimatedDuration = estimatedDuration;
         this.includesFood = includesFood;
     }
